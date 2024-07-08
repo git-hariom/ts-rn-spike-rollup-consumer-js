6bc9748b0d8bb052798ecbaaf12d26e73f158020
git-hariom <hariom.sinha.dev@gmail.com>
2024-07-08 16:14:27 +0530

    Add consumer js changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import ButtonV1 from "tsrnspikerolluppackage/components/Button";
+import { Button as ButtonV2 } from "tsrnspikerolluppackage/components/v2"; //TODO: default import????
+import { Button as ButtonV3 } from "tsrnspikerolluppackage/components/v3";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ButtonV1>ConsumerJS v1</ButtonV1>
+      <ButtonV2>ConsumerJS v2</ButtonV2>
+      <ButtonV3>ConsumerJS v3</ButtonV3>
     </div>
   );
 }
